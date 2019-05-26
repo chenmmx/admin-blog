5f75971f61e1c29bab52fb86c66dcfa92ed8f551
chenmiaoxin <1107204891@qq.com>
2019-05-26 10:36:40 +0800

    Add message list

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="layout">
+    <div class="layout-message">
+      <el-badge
+        :value="badgeNum"
+        class="item"
+      >
+        <i class="el-icon-chat-line-square" @click="showMessage"></i>
+      </el-badge>
+    </div>
     <div class="layout-left">
       <el-menu
         background-color="#f1f5f8"
@@ -34,10 +42,45 @@
       </el-menu>
     </div>
     <router-view style="width:100%;overflow-y:auto"></router-view>
+    <el-dialog
+      :title="dialogTitle"
+      :visible.sync="dialogVisible"
+      @close="handleClose"
+      width="500px">
+        <el-table
+          :data="messageList"
+          style="width: 100%">
+          <el-table-column
+            prop="username"
+            label="评论人"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="essayname"
+            label="评论文章"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            label="详情">
+            <template slot-scope="scope">
+              <a href="javascript:0;" @click="onDetail(scope.row.messageId)">查看详情</a>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-row type="flex" justify="center" style="margin-top: 10px;">
+          <el-col :span="6">
+            <el-button @click="clearAll">清除所有消息</el-button>
+          </el-col>
+        </el-row>
+    </el-dialog>
   </div>
 </template>
 <script>
 export default {
+  mounted () {
+    this.init()
+    this.getMessageList()
+  },
   data () {
     return {
       menuData: [
@@ -58,16 +101,82 @@ export default {
             { name: '角色管理' }
           ]
         }
-      ]
+      ],
+      badgeNum: 0,
+      dialogTitle: '未读消息',
+      dialogVisible: false,
+      messageList: []
     }
   },
   methods: {
+    handleClose () {},
     handleOpen (index) {
       console.log('index', index)
       if (index === '首页') {
         this.$router.push('/')
       } else if (index === '评论管理') {
         this.$router.push('/comment')
+      }
+    },
+    init () {
+      const that = this
+      const ws = new WebSocket('ws://47.112.201.254:8888')
+
+      ws.onopen = function (e) {
+        console.log('Connection to server opened')
+      }
+      ws.onmessage = function (event) {
+        console.log('Client received a message', event)
+        that.getMessageList()
+        that.$notify({
+          type: 'success',
+          title: '新的消息',
+          message: event.data
+        })
+      }
+      ws.onclose = function (e) {
+        console.log('connection closed.')
+      }
+    },
+    // 获取消息列表
+    async getMessageList () {
+      let result = await this.$http.comment.getMessage()
+      this.messageList = result.data.result.data
+      console.log(this.messageList)
+      let badgeNum = 0
+      for (let item of this.messageList) {
+        if (item.isRead === 'false') {
+          badgeNum++
+        }
+      }
+      this.badgeNum = badgeNum
+    },
+    // 清除所有消息
+    async clearAll () {
+      let result = await this.$http.comment.clearAll()
+      if (result.data.status === 0) {
+        this.$notify({
+          type: 'success',
+          title: '成功',
+          message: '清空消息成功'
+        })
+        this.getMessageList()
+        this.dialogVisible = false
+      }
+    },
+    // 显示未读消息
+    showMessage () {
+      this.dialogVisible = true
+    },
+    // 查看详情
+    async onDetail (messageId) {
+      let result = await this.$http.comment.readMessage({
+        messageId: messageId
+      })
+      if (result.data.status === 0) {
+        this.$router.push('/comment')
+        this.getMessageList()
+        this.dialogVisible = false
       }
     }
   }
@@ -88,7 +197,7 @@ export default {
       align-items: center;
       height: 200px;
       justify-content: center;
-      &>h1 {
+      & > h1 {
         color: #404d55;
       }
       & > img {
@@ -119,6 +228,18 @@ export default {
   }
   .el-submenu.is-opened .el-submenu__title {
     color: #007afe !important;
+  }
+  .layout-message {
+    position: absolute;
+    right: 30px;
+    top: 20px;
+    .el-icon-chat-line-square {
+      font-size: 25px;
+      cursor: pointer;
+      &:hover {
+        color: #007afe;
+      }
+    }
   }
 }
 </style>
--- src/service/comment/comment.js
+++ src/service/comment/comment.js
@@ -4,7 +4,10 @@ const commonUrl = '/api/comment'
 
 const url = {
   getList: commonUrl + '/getListByPage',
-  delete: commonUrl + '/delete'
+  delete: commonUrl + '/delete',
+  getMessage: commonUrl + '/getMessage',
+  clearAll: commonUrl + '/clearAll',
+  readMessage: commonUrl + '/readMessage'
 }
 export default {
   getList (data) {
@@ -12,5 +15,14 @@ export default {
   },
   delete (data) {
     return handleService(url.delete, data, 'POST')
+  },
+  getMessage (data) {
+    return handleService(url.getMessage, data)
+  },
+  clearAll (data) {
+    return handleService(url.clearAll, data)
+  },
+  readMessage (data) {
+    return handleService(url.readMessage, data, 'POST')
   }
 }
